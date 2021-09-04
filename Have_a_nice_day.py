4f0e670d266cc1ff0b0beec40d86af836bed7d18
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-09-04 09:19:51 +0300

    Create Have_a_nice_day.py

---
+++ Have_a_nice_day.py
@@ -0,0 +1,24 @@
+import time
+
+
+t1 = time.time()
+x = "Zeal and Ardor run"
+x1 = "Animals as Leaders Monomyth"
+x2 = "The disco song"
+x3 = "Tama Tama Again"
+x4 = "Let's Nacho"
+x5 = "Dream Theater Distance over time"
+x6 = "Kiko Loureiro"
+x7 = "Have a nice day!"
+
+print("cool")
+print("cool")
+print("cool")
+print("cool")
+print("cool")
+print("Love them")
+print("super cool")
+print("Have a nice day!")
+t2 = time.time()
+t = t2 - t1
+print("Elapsed time is : ", t, " seconds")
