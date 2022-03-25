ad17ccc46482ac9c8a6aa7176ae9a0f3d452bcf3
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-03-25 12:16:37 +0200

    Update Я29.pas

--- Я29.pas
+++ Я29.pas
@@ -1,8 +1,8 @@
-from urllib.request import urlopen
-page=urlopen("http://plaisio.gr/")
+#from urllib.request import urlopen
+#page=urlopen("http://plaisio.gr/")
 
 #Fetches the code
 #of the webpage
-content = page.read()
+#content = page.read()
 
-print(content)
+#print(content)
